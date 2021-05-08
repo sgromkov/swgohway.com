ad1cfa3723bd7101ff2235e419705c9a5bbbf84f
Sergey Gromkov <sgromkov@gmail.com>
2021-05-08 18:45:51 +0300

    add total found count

--- components/CharacterList.js
+++ components/CharacterList.js
@@ -1,12 +1,18 @@
 import { Divider, Box, Paper, List, ListItem, Typography } from '@material-ui/core';
 import CharacterItem from '../components/CharacterItem';
+import CustomChip from '../components/CustomChip';
 
 function CharacterList({ characters }) {
     return (
         <Box component="section" my={2}>
             <Paper>
-                <Box px={2} py={1}>
-                    <Typography variant="h4" component="h2">Characters</Typography>
+                <Box px={2} py={1} display="flex" flexWrap="nowrap" alignItems="center">
+                    <Box flexGrow="1">
+                        <Typography variant="h4" component="h2">Characters</Typography>
+                    </Box>
+                    <Box>
+                        <CustomChip label={`Found: ${characters.length}`} variant="outlined" />
+                    </Box>
                 </Box>
                 <Divider />
                 {(characters.length > 0) ? (
