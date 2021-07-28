f04068bcded22184bbcb717ce19635b96fcfd992
Sergey Gromkov <sgromkov@gmail.com>
2021-07-28 18:02:29 +0300

    reorder auth providers

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -3,10 +3,6 @@ import Providers from 'next-auth/providers';
 
 export default NextAuth({
     providers: [
-        Providers.GitHub({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET
-        }),
         Providers.Google({
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_SECRET,
@@ -14,6 +10,10 @@ export default NextAuth({
         Providers.Facebook({
             clientId: process.env.FACEBOOK_ID,
             clientSecret: process.env.FACEBOOK_SECRET
+        }),
+        Providers.GitHub({
+            clientId: process.env.GITHUB_ID,
+            clientSecret: process.env.GITHUB_SECRET
         }),
     ],
     // database: process.env.MONGODB_URI,
