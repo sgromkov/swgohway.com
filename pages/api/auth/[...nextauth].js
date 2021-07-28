d44136e624204e4ebd492d74bdb07bb0546e5bf0
Sergey Gromkov <sgromkov@gmail.com>
2021-07-28 18:01:43 +0300

    add Facebook Auth

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -11,6 +11,10 @@ export default NextAuth({
             clientId: process.env.GOOGLE_ID,
             clientSecret: process.env.GOOGLE_SECRET,
         }),
+        Providers.Facebook({
+            clientId: process.env.FACEBOOK_ID,
+            clientSecret: process.env.FACEBOOK_SECRET
+        }),
     ],
     // database: process.env.MONGODB_URI,
 });
